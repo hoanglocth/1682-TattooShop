681ca1f08c5c0cf8ff8c17f6f2f09a3902ecbf21
hoanglocth <hoanglocth@gmail.com>
2022-11-01 18:20:17 +0700

    Config Middleware

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => 'auth'], function(){
+	
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
+
+});
