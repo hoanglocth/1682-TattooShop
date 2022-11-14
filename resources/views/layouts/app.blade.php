6fd68a3c3ff984b267e975104d5d2ce4768f6803
hoanglocth <hoanglocth@gmail.com>
2022-11-14 23:15:35 +0700

    fix vite and add nav toggle

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,15 @@
         <div class="main_menu">
             <nav class="navbar navbar-expand-lg navbar-light">
                 <div class="container">
-                    <a class="navbar-brand logo_h" href="{{ route('home') }}"><img src="images/logo.png" alt=""></a>
+                    <a class="navbar-brand logo_h" href="{{ route('home') }}"><img src="/images/logo.png"
+                            alt=""></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
                     <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
                         <ul class="nav navbar-nav menu_nav ml-auto mr-auto">
                             <li class="nav-item {{ Route::currentRouteName() === 'home' ? 'active' : '' }}"><a
